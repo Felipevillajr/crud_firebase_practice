476e0f140cacccb470ee77a7369b0657316748e8
felipevillajr <felipevilla2010@live.com>
2021-05-18 15:47:26 -0400

    created env sample

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+let firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+let fireDb = firebase.initializeApp(firebaseConfig);
+
+export default fireDb.database().ref();
